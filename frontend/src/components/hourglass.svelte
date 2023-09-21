7581f8e5a929fd9985b7685a5e6482b7c6dc2eed
chayapatr <chayapatr.arc@gmail.com>
2023-09-22 03:19:39 +0700

    :tada: feat: new loading animation

---
+++ frontend/src/components/hourglass.svelte
@@ -0,0 +1,110 @@
+<div class="container flex justify-center m-3 mb-6">
+  <div class="orbit-star">
+    <div><div /></div>
+    <div><div /></div>
+  </div>
+</div>
+
+<!-- Whirl by jh3y -->
+
+<style lang="scss">
+  /**
+  * orbit-star
+  *
+  * @author Aurélio A. Heckert - http://aurium.one
+*/
+
+  $size: 100px;
+  $color: white;
+  $rock: salmon;
+
+  @keyframes orbit-star {
+    0% {
+      transform: rotate(0deg) scale(var(--scale));
+    }
+    100% {
+      transform: rotate(-360deg) scale(var(--scale));
+    }
+  }
+
+  .orbit-star,
+  .orbit-star > div {
+    --scale: 1;
+    height: $size;
+    width: $size;
+    position: relative;
+    animation: linear orbit-star 7s infinite;
+    &:before {
+      content: '';
+      display: block;
+      height: $size;
+      width: $size / 2;
+      background: transparent;
+      border-radius: $size/2 0 0 $size/2;
+      border: none;
+      border-bottom: $size/40 solid $color;
+      border-left: $size/100 solid $color;
+      border-top: 0 solid $color;
+      opacity: 0.3;
+    }
+    &:after {
+      content: '';
+      position: absolute;
+      display: block;
+      top: $size * 0.38;
+      left: $size * 0.38;
+      height: $size * 0.24;
+      width: $size * 0.24;
+      background: navajowhite;
+      border-radius: $size/6;
+    }
+  }
+
+  .orbit-star > div:nth-child(1) {
+    position: absolute;
+    top: $size * 0.25;
+    left: $size * 0.25;
+    width: $size * 0.5;
+    height: $size * 0.5;
+    animation: linear orbit-star 5s infinite;
+    &:before {
+      height: $size * 0.5;
+      width: $size * 0.25;
+      border-bottom: $size/50 solid $color;
+      border-left: $size/100 solid $color;
+    }
+    &:after {
+      top: $size * 0.44;
+      left: $size * 0.22;
+      width: $size / 12;
+      height: $size / 12;
+      background: $rock;
+    }
+  }
+
+  .orbit-star > div:nth-child(2) {
+    --scale: 0.333;
+    top: -52.5%;
+    &:before {
+      border-bottom: $size/16 solid $color;
+      border-left: $size/60 solid $color;
+    }
+    &:after {
+      top: $size * 0.29;
+      left: $size * 0.29;
+      width: $size * 0.42;
+      height: $size * 0.42;
+      border-radius: $size * 0.29;
+      background: skyblue;
+    }
+    & > div {
+      position: absolute;
+      background: lightgray;
+      width: $size / 5;
+      height: $size / 5;
+      bottom: -$size / 18;
+      left: $size * 0.4;
+      border-radius: $size/10;
+    }
+  }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
   import { api } from '$lib'
   import UploadIcon from '../components/icons/upload-icon.svelte'
+  import Hourglass from '../components/hourglass.svelte'
 
   let image: FileList | undefined
   let isLoading = false
   let result: string | undefined
+  let previewImg
 
   const process = async () => {
     if (isLoading) return
     isLoading = true
 
     if (!image) return
+
+    const preview = image[0]
+    if (preview) {
+      const reader = new FileReader()
+      reader.addEventListener('load', () => {
+        previewImg.setAttribute('src', reader.result)
+      })
+      reader.readAsDataURL(preview)
+    }
 
     result = undefined
     const { data, error } = await api.index.put({
@@ -37,16 +48,25 @@
 
   <form class="flex flex-col gap-4" on:submit|preventDefault={process}>
     <section
-      class="relative flex flex-col justify-center items-center gap-3 w-48 h-48 border-2 rounded-3xl overflow-hidden"
+      class="relative flex flex-col justify-center items-center gap-3 aspect-square w-72 border-2 rounded-3xl overflow-hidden"
     >
       <article
-        class="absolute flex flex-col justify-center items-center w-full h-full gap-1 pt-12 bg-white/80 backdrop backdrop-blur-sm p-4 transition-all {isLoading
+        class="absolute flex flex-col justify-center items-center w-full h-full bg-white/10 backdrop backdrop-blur-sm p-6 transition-all {isLoading
           ? 'z-20'
           : 'opacity-0'}"
       >
-        <span class="loading loading-infinity transform scale-[3] mb-4 mt-auto" />
-        <h2 class="text-gray-700 text-xl">Processing</h2>
-        <p class="text-gray-500">Est time: 1 minute</p>
+        <img
+          bind:this={previewImg}
+          src=""
+          alt="preview"
+          class="absolute blur-sm min-w-full min-h-full -z-20 opacity-20"
+        />
+        <Hourglass />
+        <!-- <span class="loading loading-infinity transform scale-[3] mb-4 mt-auto" /> -->
+        <div class="grid gap-1 text-center">
+          <h2 class="text-gray-200 text-xl">Getting your picture from the future...</h2>
+          <p class="text-gray-400 text-sm">Estimating time: 1 minute</p>
+        </div>
         <div class="relative w-full h-1 bg-gray-200 rounded-full mt-auto overflow-hidden">
           <div class="w-full h-full bg-black rounded-full {isLoading ? '-uploading' : ''}" />
         </div>
